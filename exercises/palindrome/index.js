fd4b6a2897923b2d02263f9571b82ac97cc0cb43
Tayfur Erkenci <terkenci@gmail.com>
2022-05-15 20:59:12 +0300

    palindrome resolved

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return  str.split('').every((char, i) => char === str[str.length - i - 1]);
+}
 
 module.exports = palindrome;
+
+// sltn: 1
+// function palindrome(str) {
+//  const reversed = str.split('').reduce((rev, char) => char + rev, '');
+//  return str === reversed;
+// }
