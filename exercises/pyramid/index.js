8d41092678b5de99f5868c01c12c5cb803d9b710
Tayfur Erkenci <terkenci@gmail.com>
2022-05-23 00:41:21 +0300

    pyramid resolved

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -10,10 +10,49 @@
 //       ' # '
 //       '###'
 //   pyramid(3)
-//       '  #  '
-//       ' ### '
-//       '#####'
+//       '  #  ' row 1 col 1
+//       ' ### ' row 2 col 3
+//       '#####' row 3 col 5
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level='') {
+
+  if(row === n){
+    return;
+  }
+
+  if(2*n-1 === level.length ){
+    console.log(level);
+    return pyramid(n, row+1);
+  }
+
+  const midpoint = Math.floor((2*n-1) / 2);
+
+  const add = midpoint - row <= level.length && midpoint + row >= level.length ? "#" : " ";
+
+  pyramid(n, row, level + add);
+
+}
 
 module.exports = pyramid;
+
+
+// for loop inside for loop approach :
+// function pyramid(n) {
+//   const midpoint = Math.floor((2*n-1) / 2);
+//
+//   for(let row=0; row < n; row++)
+//   {
+//     let level='';
+//     for(let column = 0; column< 2 * n - 1; column++)
+//     {
+//       if(midpoint - row <= column && midpoint + row >= column)
+//       {
+//         level+="#";
+//       }
+//       else {
+//         level+=" ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
