5f1ca010f6d9976b63879d2f07cc29c42a9c8d50
Tayfur Erkenci <terkenci@gmail.com>
2022-05-22 23:32:53 +0300

    steps resolved

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,48 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+  if(n === row)
+  {
+    return;
+  }
+
+  if(n === stair.length)
+  {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  // const add = stair.length <= row ? '#' : ' ';
+  if(stair.length <= row)
+  {
+    stair+="#";
+  } else {
+    stair +=" ";
+  }
+  steps(n, row, stair);
+
+}
 
 module.exports = steps;
+
+
+// alternativa solution :
+// function steps(n) {
+//   for (let i = 0; i < n; i++) {
+//
+//     let line='';
+//
+//     for(let j = 0; j < n; j++){
+//
+//       if(j <= i){
+//         line+="#";
+//       }
+//       else {
+//         line+=' ';
+//       }
+//
+//     }
+//     console.log(line);
+//   }
+// }
