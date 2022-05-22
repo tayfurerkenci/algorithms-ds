0ab4246737f100e04a85df82a1d3619c04e5431d
Tayfur Erkenci <terkenci@gmail.com>
2022-05-22 19:46:34 +0300

    capitalize resolved

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,29 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const capitalize=[];
+  
+  for (var word of str.split(' ')) {
+    capitalize.push(word[0].toUpperCase()+ word.slice(1));
+  }
+
+  return capitalize.join(' ');
+}
 
 module.exports = capitalize;
+
+// solution alternative:
+// function capitalize(str) {
+//   let result=str[0].toUpperCase();
+//
+//   for (let i = 1; i < str.length; i++) {
+//     if(str[i - 1] === ' '){
+//       result+= str[i].toUpperCase();
+//     }else{
+//       result += str[i];
+//     }
+//   }
+//
+//   return result;
+// }
