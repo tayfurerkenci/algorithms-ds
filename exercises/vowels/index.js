dfb0d57072e0c721c240ba8c9c9f38a01ff43242
Tayfur Erkenci <terkenci@gmail.com>
2022-05-23 22:46:43 +0300

    vowels resolved

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,27 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // if it finds any matches, it will return an
+  // array of all the matches that were found
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+
+// iterative solution :
+// function vowels(str) {
+//   const vowel = ["a", "o", "e", "u", "i"];
+//   let count=0;
+//
+//   for(let char of str.toLowerCase())
+//   {
+//     if(vowel.includes(char))
+//     {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
